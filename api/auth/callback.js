1d61a550847cafd39a973b51c2f7a24edfbac546
MichaelKaffel <michaeldkaffel@gmail.com>
2026-04-10 14:45:12 -0700

    feat: add Decap CMS admin UI and Github OAuth auth endpoints

---
+++ api/auth/callback.js
@@ -0,0 +1,40 @@
+const handler = async (req, res) => {
+    const { code } = req.query;
+
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code,
+        }),
+    });
+
+    const data = await response.json();
+
+    if (data.error) {
+        return res.status(400).send(`
+            <script>
+            window.opener.postMessage(
+            'authorization:github:error:${data.error}',
+            '*'
+            );
+            </script>
+            `);
+    }
+
+    return res.status(200).send(`
+         <script>
+         window.opener.postMessage(
+         'authorization:github:success:${JSON.stringify({ token: data.access_token, provider: 'github' })}',
+         '*'
+         );
+         </script>
+    `);
+}
+
+export default handler;
